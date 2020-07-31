29500ab6481f5ebbbfedf13a761ba466d077b2c5
ethanrasmussen <itseraz@gmail.com>
2020-07-31 09:11:59 -0500

    add online status to embed

--- utils/Course.py
+++ utils/Course.py
@@ -47,6 +47,7 @@ class Course:
             embed.add_field(name='Degree Attributes', value=self.deg_attr, inline=False)
 
         embed.add_field(name='Status', value=self.status, inline=False)
+        embed.add_field(name='Online/Offline Status', value=self.online_status, inline=False)
         # print(repr(self.status))
         if random.random() < 0.33:
             embed.set_footer(text=random.choice(mistakes) + ' DM @10x engineer#9075.')
